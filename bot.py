7ce6113f45a6c44aec032b6682090b5677c97b4f
Balaram Usov <dest@disroot.org>
2019-11-29 22:02:54 +0300

    Added location handling

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton
 from telegram.utils.request import Request
 
 import config
 
+import json
+import datetime
 
 req = Request(proxy_url=config.proxy)
 bot = Bot(config.token, request=req)
@@ -17,12 +19,24 @@ def start(update, context):
 
 def photo(update, context):
     print(update.message.photo)
+    request_location(context.bot, chat_id=update.effective_chat.id)
 
-start_handler = CommandHandler('start', start)
-photo_handler = MessageHandler(Filters.photo, photo)
+def request_location(bot, chat_id):
+    location_keyboard = KeyboardButton(text="Send location", request_location=True)
+    custom_keyboard = [[ location_keyboard ]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=chat_id,
+                     text="Would you mind sharing your location with me?",
+                     reply_markup=reply_markup)
 
-dp.add_handler(start_handler)
-dp.add_handler(photo_handler)
+def location(update, context):
+    print(update.message.location)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Thank you for sharing location')
+
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.photo, photo))
+dp.add_handler(MessageHandler(Filters.location, location))
 
 def main():
     upd.start_polling()
