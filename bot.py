326229cf07ef09cfb4d6dc16e75317cdae5e0386
Nataliya Tyshkevich <natasha@api.ai>
2019-11-29 21:52:35 +0300

    added photo_handler, voice_handler

--- bot.py
+++ bot.py
@@ -16,13 +16,31 @@ def start(update, context):
                              text="Hello! My name is {0}".format(config.name))
 
 def photo(update, context):
-    print(update.message.photo)
+    file_id = update.message.photo[-1].file_id
+    newFile = bot.get_file(file_id)
+    print(file_id)
+    newFile.download(str(file_id) + '.jpg')
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Thanks for sharing the picture!")
+                             
+def voice(update, context):
+    file_id = update.message.voice.file_id
+    newFile = bot.get_file(file_id)
+    print(file_id)
+    newFile.download(str(file_id) + '.ogg')
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Thanks for sharing the audio!")
+
+    
+    
 
 start_handler = CommandHandler('start', start)
 photo_handler = MessageHandler(Filters.photo, photo)
+voice_handler = MessageHandler(Filters.voice, voice)
 
 dp.add_handler(start_handler)
 dp.add_handler(photo_handler)
+dp.add_handler(voice_handler)
 
 def main():
     upd.start_polling()
