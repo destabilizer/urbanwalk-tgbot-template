c25c00a1a157ddd8fd18869a157876a86e0e2172
Nataliya Tyshkevich <natasha@api.ai>
2019-11-30 04:19:50 +0300

    csv dumper

--- dump.py
+++ dump.py
@@ -7,21 +7,36 @@ import os
 datadir = os.path.join(os.path.expanduser('~'), 'data', config.name.lower().replace(' ', '_'))
 if not os.path.exists(datadir): os.makedirs(datadir)
 
+logf = open(os.path.join(datadir,'dump.csv'), 'w')
+
 def fnpath(username, ts, ft, fmt):
     return os.path.join(datadir, '{0}_{1}_{2}.{3}'.format(username, ts, ft, fmt))
-
+    
+#locf = open(fnpath(username, ts, filetype, 'csv'), 'w')
 def data_with_location(filetype, data, username, location):
+    logf = open(os.path.join(datadir,'dump.csv'), 'a')
     print('going to save data', filetype, data, username, location)
     ts = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
     if filetype == 'photo':
-        data.download(fnpath(username, ts, filetype, 'jpg'))
+        f = fnpath(username, ts, filetype, 'jpg')
+        data.download(f)
     elif filetype == 'voice':
-        data.download(fnpath(username, ts, filetype, 'ogg'))
+        f = fnpath(username, ts, filetype, 'ogg')
+        data.download(f)
     elif filetype == 'text':
-        with open(fnpath(username, ts, filetype, 'txt')) as textf:
+        f = fnpath(username, ts, filetype, 'txt')
+        with open(f) as textf:
             textf.write(data)
     else:
+        f = ''
         raise TypeError('wrong filetype')
-    with open(fnpath(username, ts, filetype, 'json'), 'w') as locf:
-        locf.write(json.dumps({'longitude': location.longitude,
-                               'latitude': location.latitude}, indent=2))
+#        locf.write(json.dumps({'filename': f,
+#                               'longitude': location.longitude,
+#                               'latitude': location.latitude,
+#                               'timestamp': ts,
+#                               'filetype': filetype,
+#                               'username': username}, indent=2))
+    logf.write("id,longitude,latitude,timestamp,filetype,username\n")
+    logf.write(str(f)+','+str(location.longitude)+','+str(location.latitude)+','+str(ts)+','+str(filetype)+','+str(username)+'\n')
+    logf.flush()
+	
